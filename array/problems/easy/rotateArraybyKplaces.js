e3808c4ddce198d51abfb76ab32c2a7df44ca477
Ubaid Ali <aliubaid534@gmail.com>
2026-07-04 01:03:49 +0530

    rotating array right

---
+++ array/problems/easy/rotateArraybyKplaces.js
@@ -0,0 +1,31 @@
+// question: Rotate an array by K places to the right
+
+// -------------------------rotate array by K places-------------------------//
+function rotateArray(arr, k) {
+  let n = arr.length;
+  let temp = [];
+  for (let i = 0; i < k; i++) {
+    temp[i] = arr[i];
+  }
+  //time complexity is O(k) and space complexity is O(k) because we are using an extra array of size k to store the last k elements of the original array.
+
+  for (let i = k; i < n; i++) {
+    arr[i - k] = arr[i];
+  }
+  //time complexity is O(n - k) and space complexity is O(1) because we are not using any extra space.
+
+  for (let i = n - k; i < n; i++) {
+    arr[i] = temp[i - (n - k)]; //i - (n - k) gives the zero-based index of the temp array, otherwise, it would be out of bounds.
+  }
+  //time complexity is O(k) and space complexity is O(1) because we are not using any extra space.
+
+  return arr;
+}
+
+//time complexity is O(n) + O(k) and space complexity is O(k) because we are using an extra array
+// of size k to store the last k elements of the original array.
+
+let arr = [1, 2, 3, 4, 5];
+let k = 2;
+let result = rotateArray(arr, k);
+console.log(`Array after rotating by ${k} places: ${result}`);
