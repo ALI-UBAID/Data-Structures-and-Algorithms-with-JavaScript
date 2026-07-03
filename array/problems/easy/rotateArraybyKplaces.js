b34ae5dfa03cb23056048c8fb1800eb19dea8fac
Ubaid Ali <aliubaid534@gmail.com>
2026-07-04 01:12:59 +0530

    update

--- array/problems/easy/rotateArraybyKplaces.js
+++ array/problems/easy/rotateArraybyKplaces.js
@@ -28,7 +28,31 @@ function rotateArray(arr, k) {
 //time complexity is O(n) + O(k) and space complexity is O(k) because we are using an extra array
 // of size k to store the last k elements of the original array.
 
+// --------------------------optimal solution-------------------------//
+// To rotate an array by K places to the right, we can reverse the entire array,
+// then reverse the first K elements and finally reverse the remaining elements.
+function reverse(arr, start, end) {
+  while (start <= end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+function rotateArrayOptimal(arr, k) {
+  let n = arr.length;
+  k = k % n; // to handle cases where k is greater than n
+  reverse(arr, 0, k - 1);
+  reverse(arr, k, n - 1);
+  reverse(arr, 0, n - 1);
+  return arr;
+}
+// time complexity is O(n) and space complexity is O(1) because we are not using any extra space.
+
 let arr = [1, 2, 3, 4, 5];
 let k = 2;
-let result = rotateArray(arr, k);
+// let result = rotateArray(arr, k);
+let result = rotateArrayOptimal(arr, k);
 console.log(`Array after rotating by ${k} places: ${result}`);
