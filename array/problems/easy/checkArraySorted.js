a057d759cbdb7c0597423835ac557dfea13a949c
Ubaid Ali <aliubaid534@gmail.com>
2026-07-03 23:56:30 +0530

    array sort check

---
+++ array/problems/easy/checkArraySorted.js
@@ -0,0 +1,14 @@
+function sortCheck(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+// time complexity is O(n) and space complexity is O(1) because we are not using any extra space.
+
+let arr = [1, 2, 3, 4, 5];
+let result = sortCheck(arr);
+console.log(`Is the array sorted? ${result}`);
